62e6ba2a82836e63c862f38484f86b9608be5dc8
Tim Moore <tim.moore@lightbend.com>
2018-04-12 14:45:05 +0930

    Modify tests to use a shared server
    As expected, this causes the second test case to fail.

--- hello-impl/src/test/java/com/example/hello/impl/HelloServiceTest.java
+++ hello-impl/src/test/java/com/example/hello/impl/HelloServiceTest.java
@@ -2,43 +2,57 @@ package com.example.hello.impl;
 
 import com.example.hello.api.GreetingMessage;
 import com.example.hello.api.HelloService;
+import com.lightbend.lagom.javadsl.testkit.ServiceTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
-import static com.lightbend.lagom.javadsl.testkit.ServiceTest.withServer;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.Assert.assertEquals;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HelloServiceTest {
 
+  private static final ServiceTest.Setup setup = defaultSetup().withCassandra();
+  private static ServiceTest.TestServer server;
+
+  @BeforeClass
+  public static void startServer() {
+    server = ServiceTest.startServer(setup);
+  }
+
+  @AfterClass
+  public static void stopServer() {
+    if (server != null) {
+      server.stop();
+    }
+    server = null;
+  }
+
   @Test
   public void should1StorePersonalizedGreeting() throws Exception {
-    withServer(defaultSetup().withCassandra(), server -> {
-      HelloService service = server.client(HelloService.class);
+    HelloService service = server.client(HelloService.class);
 
-      String msg1 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hello, Alice!", msg1); // default greeting
+    String msg1 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
+    assertEquals("Hello, Alice!", msg1); // default greeting
 
-      service.useGreeting("Alice").invoke(new GreetingMessage("Hi")).toCompletableFuture().get(5, SECONDS);
-      String msg2 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hi, Alice!", msg2);
+    service.useGreeting("Alice").invoke(new GreetingMessage("Hi")).toCompletableFuture().get(5, SECONDS);
+    String msg2 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
+    assertEquals("Hi, Alice!", msg2);
 
-      String msg3 = service.hello("Bob").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hello, Bob!", msg3); // default greeting
-    });
+    String msg3 = service.hello("Bob").invoke().toCompletableFuture().get(5, SECONDS);
+    assertEquals("Hello, Bob!", msg3); // default greeting
   }
 
   @Test
   public void should2IsolateTestData() throws Exception {
-    withServer(defaultSetup().withCassandra(), server -> {
-      HelloService service = server.client(HelloService.class);
+    HelloService service = server.client(HelloService.class);
 
-      String msg1 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
-      assertEquals("Hello, Alice!", msg1); // default greeting, not affected by the change in the previous test
-    });
+    String msg1 = service.hello("Alice").invoke().toCompletableFuture().get(5, SECONDS);
+    assertEquals("Hello, Alice!", msg1); // default greeting, not affected by the change in the previous test
   }
 
 }
